949e23f01a260750771ce0ac1ec623a4926ee334
obaid78-coder <urajaobaidgmail.com>
2026-05-08 03:48:22 +0500

    fix indent

--- app.py
+++ app.py
@@ -381,4 +381,4 @@ def search_suggestions():
 if __name__ == '__main__':
     init_db()
     port = int(os.environ.get('PORT', 5050))
-app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port)
